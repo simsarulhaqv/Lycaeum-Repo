a84498854cc076b7857d9049b08846120b7f2ecc
simsarulhaqv <simsarulhaqv@gmail.com>
2015-11-12 19:58:19 +0530

    Primality() Incorrect- Need fix

--- pythonPracticeProblems/primality.py
+++ pythonPracticeProblems/primality.py
@@ -1,15 +1,26 @@
-# Primality
+# Primality 
 
-x = [0 for x in range(100)]
+# modify size to change list length
+size = 100
+# initialize list with zeroes
+x = [0 for x in range(101)]
 
-def primality(number,count) :
-	for index in x:
-		if (index != 0) :
-			if (number % index) == 0 :
-				return "NIL",count
-	else :	
-		x[count] = number
-		count = count + 1		
-		return number,count
+# primality function
+def primality(number) :
+    if (number not in x) :
+        value = number
+        counter = 1
+        while(value <= 100) :
+            value = counter*number
+            x[value] = value 
+            counter = counter + 1
+        return number
+    else :	
+        return "NIL",count
 
 
+number = input("range ")
+for i in range(2,number):
+	ans=primality(i)
+	if ( ans != "NIL") :
+ 		print ans
