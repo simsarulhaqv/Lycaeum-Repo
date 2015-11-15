d28a38fe176cc016af2885d5028abdd0cac197ab
simsarulhaqv <simsarulhaqv@gmail.com>
2015-11-15 22:37:59 +0530

    Added getNeighbour()

--- gameOfLife/gameOfLife.py
+++ gameOfLife/gameOfLife.py
@@ -1,14 +1,34 @@
+SIZE = 3;
+
 def getMatrix():
-    # return a population
+    # return a population of size SIZE
     return [[1,0,1],[1,0,0],[1,0,1]]
 
-def getCell(count,matrix) :
-    row = count/3
-    column = count%3
+def getCell(number,matrix) :
+    row = number/SIZE
+    column = number%SIZE
     return matrix[row][column]
 
-def getNeighbours(row,column,matrix) :
+def getNeighbours(number,matrix) :
+    row = number/SIZE
+    column = number%SIZE
     # -1 means no neighbour
+    count = 0
     neighbour = [-1 for i in range(8)]
-    # need to complete
+    neighbour[count] = (((row-1)*SIZE)%SIZE) + ((column-1)%SIZE)
+    count = count + 1
+    neighbour[count] = (((row-1)*SIZE)%SIZE) + ((column)%SIZE)
+    count = count + 1]
+    neighbour[count] = (((row-1)*SIZE)%SIZE) + ((column+1)%SIZE)
+    count = count + 1
+    neighbour[count] = (((row+1)*SIZE)%SIZE) + ((column+1)%SIZE)
+    count = count + 1
+    neighbour[count] = (((row+1)*SIZE)%SIZE) + ((column)%SIZE)
+    count = count + 1
+    neighbour[count] = (((row+1)*SIZE)%SIZE) + ((column+1)%SIZE)
+    count = count + 1
+    neighbour[count] = (((row)*SIZE)%SIZE) + ((column-1)%SIZE)
+    count = count + 1
+    neighbour[count] = (((row)*SIZE)%SIZE) + ((column+1)%SIZE)
+    return neighbour
     
