fbe721d51799ce124f618a4da8a9cb51e72172a8
simsarulhaqv <simsarulhaqv@gmail.com>
2015-11-15 16:26:40 +0530

    removed dataStruct.py

--- evaluatePostfix/dataStruct.py
+++
@@ -1,9 +0,0 @@
-class Stack :
-    def __init__(self) :
-        self.item=[]
-    def push(self,value) :
-        self.item = self.item.append(value)
-    def pop(self) :
-        return self.item.pop()
-    def isEmpty(self) :
-        return self.item == []
